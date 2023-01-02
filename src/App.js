831498100eebdc4946af3f968d0dc6c422e10b6b
MartyDude20 <martavious.hails@gmail.com>
2023-01-02 14:53:14 -0600

    Restr. index.js to have route for multiple pages

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+import './css/index.css';
+import NameInput from './nameInput';
+// ========================================
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<NameInput />);
+
+const App = () => {
+    return (
+      <div>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route exact path="/" element={<NameInput />} />
+          {/* <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create />} /> */}
+        </Routes>
+      </div>
+    );
+   };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import Game from './game';
-import NameInput from './nameInput';
-// ========================================
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<NameInput />);
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
